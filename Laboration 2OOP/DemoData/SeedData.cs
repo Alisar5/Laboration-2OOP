d60fad3a39f690db8b86a59fbd9eddfd526f92c7
Helin Akgül <s2508321@student.hb.se>
2026-06-07 05:19:45 +0200

    Lagt in klassen seeddata på demodata

---
+++ Laboration 2OOP/DemoData/SeedData.cs
@@ -0,0 +1,12 @@
+﻿using Laboration_2OOP.DemoData;
+using Laboration_2OOP.Domän;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_2OOP.DemoData
+{
+}
+
