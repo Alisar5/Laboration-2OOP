97a482af9b54d38326c33d395d52e706986bb8a2
Helin Akgül <s2508321@student.hb.se>
2026-06-08 17:08:29 +0200

    Fixade till using cases.

--- Laboration 2OOP/Views/MainWindow.xaml.cs
+++ Laboration 2OOP/Views/MainWindow.xaml.cs
@@ -1,22 +1,9 @@
-﻿using Laboration_2OOP.DemoData;
-using Laboration_2OOP.Domän;
-using Laboration_2OOP.Requests;
-using Laboration_2OOP.Services;
+﻿
 using Laboration_2OOP.ViewModels;
 using System;
-using System.Collections.ObjectModel;
-using System.Data;
-using System.Globalization;
-using System.Text;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Laboration_2OOP
 {
